a6acccc0b8eb1ca052807c661c7912d8b9402fb4
胡国彪 <huguobiao@360.cn>
2020-08-30 23:15:07 +0800

    init

---
+++ src/main/java/peoplesoft/jwt/utils/ToolUtil.java
@@ -0,0 +1,21 @@
+package peoplesoft.jwt.utils;
+
+import java.util.Random;
+
+public class ToolUtil {
+
+	/**
+	 * 获取随机位数的字符串
+	 */
+	public static String getRandomString(int length) {
+		String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+		Random random = new Random();
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < length; i++) {
+			int number = random.nextInt(base.length());
+			sb.append(base.charAt(number));
+		}
+		return sb.toString();
+	}
+
+}
